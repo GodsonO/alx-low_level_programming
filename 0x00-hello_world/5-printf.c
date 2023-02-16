c3bb8b92ce10c57182d29c061cfc2b75d82cb683
GodsonO <godsonomoghene15@gmail.com>
2023-02-16 04:16:07 -0800

    5-prints.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A C programmimg language that prints a line using the printf function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,");
+	return (0);
+}
