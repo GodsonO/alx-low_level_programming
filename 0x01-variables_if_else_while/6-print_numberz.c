d21ce74c09f19cd49a4111815a1e818405128854
GodsonO <godsonomoghene15@gmail.com>
2023-02-17 09:15:40 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints numbers of base 10 starting from 0
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	x = 0;
+	while
+		(x < 10) {
+			putchar(x);
+			x++;
+		}
+	putchar('\n');
+	return (0);
+}
