edf3d629a026afa7298514e8f835078d558555c9
GodsonO <godsonomoghene15@gmail.com>
2023-03-01 10:18:58 -0800

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *cap_string - capitalizes all words of a string
+ * @n: the string
+ * Return: a char
+ */
+char *cap_string(char *n)
+{
+	int a = 0;
+
+	while (n[a])
+	{
+		while (!(n[a] >= 'a' && n[a] <= 'z'))
+			a++;
+		if (n[a - 1] == ' ' || n[a - 1] == '\t' || n[a - 1] == '\n' ||
+			n[a - 1] == ',' || n[a - 1] == ';' || n[a - 1] == '.' ||
+			n[a - 1] == '!' || n[a - 1] == '?' || n[a - 1] == '"' ||
+			n[a - 1] == '(' || n[a - 1] == ')' || n[a - 1] == '{' ||
+			n[a - 1] == '}' || a == 0)
+		{
+			n[a] -= 32;
+			a++;
+		}
+	}
+	return (n);
+}	
