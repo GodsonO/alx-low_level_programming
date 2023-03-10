25aeb02010fe41f9fa98405f20487d14ea94669c
GodsonO <godsonomoghene15@gmail.com>
2023-03-10 09:35:40 -0800

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - a program that adds positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	unsigned int x, sum = 0;
+	char *n;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			n = argv[i];
+
+			for (x = 0; x < strlen(n); x++)
+			{
+				if (n[x] < 48 || n[x] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum += atoi(n);
+			n++;
+		}
+		printf("%d/n", sum);
+	}
+	else
+	{
+		printf("0\n");
+	}
+	return (0);
+}
