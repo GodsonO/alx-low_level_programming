7535b7a686fa4904d835929f3a6d4dbaf23fcf8f
GodsonO <godsonomoghene15@gmail.com>
2023-07-07 19:34:57 -0700

    hash tables

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -4,7 +4,6 @@
  * key_index - gives the index of a key
  * @key: key to get index for
  * @size: size of the hash table
- *
  * Return: index for the key
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
