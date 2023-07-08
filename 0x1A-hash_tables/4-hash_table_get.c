c4d8ee0e2b7b52a9047ccad04d5eb08c7c48c52d
GodsonO <godsonomoghene15@gmail.com>
2023-07-07 19:49:15 -0700

    hash tables

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -4,7 +4,6 @@
  * hash_table_get - retrieves a value associated with a key
  * @ht: table to retrieve value from
  * @key: key to find value
- *
  * Return: value associated with key, or NULL if key cannot be found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
